699aebd3dd3c65960284c22af70d855e0ca7ea37
anitaSzabo <azorka3@gmail.com>
2018-09-17 14:43:23 +0200

    QuestionController and UserController classes added

---
+++ src/com/codecool/kakook/UserController.java
@@ -0,0 +1,14 @@
+package com.codecool.kakook;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserController {
+
+    private List<User> users = new ArrayList<>();
+
+
+    private void getRank() {
+
+    }
+}
