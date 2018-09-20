633431d78f476cd860d368f432dd90fd35bc7b31
Milan Kiszely <milankiszely@gmail.com>
2018-09-20 10:42:53 +0200

    admin front-end developed

--- src/main/webapp/static/js/adminnarrative.js
+++ src/main/webapp/static/js/adminnarrative.js
@@ -9,11 +9,18 @@ let narrative = {
     on_new_question: function (question, answers) {
         $(".game-starting").hide();
         $(".question-wrapper .question").html(question);
-        let i = 0;
-        for (let answer in answers) {
-            $("#answer" + i).html(answer);
+        var i = 0;
+        for (var answer in answers) {
+            $("#answer" + i).html(answer).css("opacity", 1);
             i++;
         }
         $(".question-wrapper").show();
+    },
+    on_answer: function (good_answer_number, answer_statistic) {
+        for (let i = 1; i <= 4; i++){
+            if (i !== good_answer_number)
+                $("#answer" + i).css("opacity", 0.5);
+        }
+
     }
 };
